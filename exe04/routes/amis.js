ffa3e05042daf6989aaf677b59cefd5e3333960f
manoi21 <sbeguin@free.fr>
2018-01-16 16:15:59 +0100

    test000

--- exe04/routes/amis.js
+++ exe04/routes/amis.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 module.exports = (req,res) =>{
-    fs.readFile(`${process.cwd()}/public/friends.json`, {encoding: 'utf-8'},(err,data)=>{
+    fs.readFile(`${process.cwd()}/public/amis.json`, {encoding: 'utf-8'},(err,data)=>{
+        console.log(data)
         if(err) {
             res.writeHead(500,{'Content-Type':'text/html'})
             res.end('The server has a problem try later')
@@ -11,8 +12,19 @@ module.exports = (req,res) =>{
                     res.end('File Not Found')
             } else {
                 res.end(generateHtml(data,json))
-            }
+            } 
         })
     }
     })
+} 
+const generateHtml = (tpl,json) =>{
+        // let values = JSON.parse(json)
+        // let tempArr = values.map(item =>item.name)
+        // console.log(tempArr)
+        // let tempStr = tempArr.join('</li><li>')
+        // console.log(tempStr)
+        // let htmlStr = tpl.replace('%amis%',tempStr)
+        // console.log(htmlStr)
+        // return htmlStr
+        return tpl.replace('%amis%',JSON.parse(json).map(item =>item.name).join('</li><li>'))
 }
